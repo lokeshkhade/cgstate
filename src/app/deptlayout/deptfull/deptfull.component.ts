ef043328f02e9b9a91d5c4f9af8db9c3aaa4a934
lokeshkhade <lokeshkhade@gmail.com>
2021-06-18 07:34:15 +0530

    18-06-2021 CGSTATE

--- src/app/deptlayout/deptfull/deptfull.component.ts
+++ src/app/deptlayout/deptfull/deptfull.component.ts
@@ -1,17 +1,71 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { CommonService } from './../../services/common.service';
+import { DatePipe } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { OwlOptions } from 'ngx-owl-carousel-o';
+
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-deptfull',
   templateUrl: './deptfull.component.html',
   styleUrls: ['./deptfull.component.scss']
 })
+
+
 export class DeptfullComponent implements OnInit {
   public domain_name: any;
-  constructor(private router: Router, private route: ActivatedRoute) { }
+
+  news: any = [];
+  data: any = [];
+  deptdata: any = [];
+  impldata: any = [];
+  currentdate: any;
+  yesterday: any;
+  public dept_id: any;
+  rootUrl = environment.rootUrl;
+  public carousel: any = [
+    {
+      img: 'assets/images/carousel/01.jpg',
+      alt: '',
+      heading: 'State Capital Raipur'
+    }, {
+      img: 'assets/images/carousel/02.jpg',
+      alt: '',
+      heading: 'The Tribes of Chhattisgarh'
+    }, {
+      img: 'assets/images/carousel/03.jpg',
+      alt: '',
+      heading: 'Craft of Chhattisgarh'
+    }, {
+      img: 'assets/images/carousel/04.jpg',
+      alt: '',
+      heading: 'Places of Chhattisgarh'
+    },
+  ]
+
+  constructor(private http: HttpClient, private dp: DatePipe, private commonservice: CommonService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.domain_name = this.route.snapshot.paramMap.get('domain_name');
+    console.log(this.domain_name);
+    this.getDeptID();
+  }
+
+  getDeptID() {
+
+    this.commonservice.paramFunction('deptidbydomainname', this.domain_name).subscribe(res => {
+      this.dept_id = res[0].dept_id;
+      this.getDeptData(this.dept_id);
+
+    });
+  }
+
+  getDeptData(dept_id: any) {
+    this.commonservice.paramFunction('dept', this.dept_id).subscribe(res => {
+      this.deptdata = res[0];
+    });
   }
 
 }
--- src/app/deptlayout/depthome/depthome.component.ts
+++ src/app/deptlayout/depthome/depthome.component.ts
@@ -2,44 +2,29 @@ import { CommonService } from './../../services/common.service';
 import { DatePipe } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { OwlOptions } from 'ngx-owl-carousel-o';
-
 import { environment } from 'src/environments/environment';
 import { ActivatedRoute, Router } from '@angular/router';
+
+
 @Component({
   selector: 'app-depthome',
   templateUrl: './depthome.component.html',
   styleUrls: ['./depthome.component.scss']
 })
 export class DepthomeComponent implements OnInit {
+
+  public domain_name: any;
   news: any = [];
   data: any = [];
-  deptdata: any = [];
   impldata: any = [];
   currentdate: any;
   yesterday: any;
-  public domain_name: any;
   public dept_id: any;
+  deptdata: any = [];
   rootUrl = environment.rootUrl;
-  public carousel: any = [
-    {
-      img: 'assets/images/carousel/01.jpg',
-      alt: '',
-      heading: 'State Capital Raipur'
-    }, {
-      img: 'assets/images/carousel/02.jpg',
-      alt: '',
-      heading: 'The Tribes of Chhattisgarh'
-    }, {
-      img: 'assets/images/carousel/03.jpg',
-      alt: '',
-      heading: 'Craft of Chhattisgarh'
-    }, {
-      img: 'assets/images/carousel/04.jpg',
-      alt: '',
-      heading: 'Places of Chhattisgarh'
-    },
-  ]
+  public gallery: any = [];
+  public carousel: any = [];
+
   constructor(private http: HttpClient, private dp: DatePipe, private commonservice: CommonService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
@@ -48,26 +33,40 @@ export class DepthomeComponent implements OnInit {
     console.log(this.domain_name);
     this.getDeptID();
 
-
-    this.currentdate = this.dp.transform(new Date().toISOString(), 'dd/MM/yyyy');
-    this.yesterday = this.dp.transform((d => new Date(d.setDate(d.getDate() - 1)))(new Date).toISOString(), 'dd/MM/yyyy');
-
-    this.getNews(this.currentdate);
-
-    this.commonservice.getFunction('noticeboard').subscribe(res => {
-      this.data = res;
-    });
-
-    this.commonservice.getFunction('impinformation').subscribe(res => {
-      this.impldata = res;
-    });
-
   }
+
   getDeptID() {
 
     this.commonservice.paramFunction('deptidbydomainname', this.domain_name).subscribe(res => {
       this.dept_id = res[0].dept_id;
       this.getDeptData(this.dept_id);
+
+      /////////////////////////////////////////////////
+
+      this.getDeptData(this.dept_id);
+      this.getDeptbanner(this.dept_id);
+      this.getDeptgallery(this.dept_id);
+
+      this.commonservice.getFunction('noticeboard').subscribe(res => {
+        this.data = res;
+      });
+
+
+
+      this.commonservice.paramFunction('impinformation', this.dept_id).subscribe(res => {
+        this.impldata = res;
+        console.log(this.impldata);
+      });
+
+
+
+      //////////////////////////////////////////////////////////////
+
+
+
+
+
+
 
     });
   }
@@ -79,41 +78,21 @@ export class DepthomeComponent implements OnInit {
   }
 
 
-
-  getNews(date: any) {
-    this.http.get('http://dprcg.gov.in/api/aajKeSamachar?date=' + date).subscribe((res: any) => {
-      this.news = res;
-      if (this.news.length == 0) {
-        this.getNews(this.yesterday);
-      }
+  getDeptbanner(dept_id: any) {
+    this.commonservice.paramFunction('banner', this.dept_id).subscribe(res => {
+      this.carousel = res;
     });
   }
 
-  customOptions: OwlOptions = {
-    loop: true,
-    autoplay: true,
-    mouseDrag: true,
-    touchDrag: true,
-    pullDrag: true,
-    dots: false,
-    navSpeed: 700,
-    navText: ['', ''],
-    items: 4,
-    margin: 10,
-    autoplayTimeout: 1000,
-    autoplayHoverPause: true,
-    responsive: {
-      0: {
-        items: 1
-      },
-      400: {
-        items: 2
-      },
-      740: {
-        items: 4
-      }
-    },
-    nav: true
+  getDeptgallery(dept_id: any) {
+    this.commonservice.paramFunction('gallery', this.dept_id).subscribe(res => {
+      this.gallery = res;
+    });
   }
+
+
+
+
+
 
 }
